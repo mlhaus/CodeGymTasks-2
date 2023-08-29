2375ce9ea70dbdda1f810ed15a5232412d3566e6
Marc Hauschildt <marc.hauschildt@gmail.com>
2023-08-29 18:51:18 -0500

    Completed task 1001

---
+++ 1.JavaSyntax/src/en/codegym/task/pro/task10/task1001/Skyscraper.java
@@ -0,0 +1,19 @@
+package en.codegym.task.pro.task10.task1001;
+
+/* 
+Creating a skyscraper
+*/
+
+public class Skyscraper {
+    
+    public Skyscraper() {
+        System.out.println("The skyscraper was built.");
+    }
+
+    //write your code here
+
+    public static void main(String[] args) {
+        //write your code here
+        new Skyscraper();
+    }
+}
