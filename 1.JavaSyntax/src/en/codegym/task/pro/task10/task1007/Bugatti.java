94a985eea690a51ec601590821b49bf4332600dc
Marc Hauschildt <marc.hauschildt@gmail.com>
2023-08-29 19:58:44 -0500

    Completed task 1007

---
+++ 1.JavaSyntax/src/en/codegym/task/pro/task10/task1007/Bugatti.java
@@ -0,0 +1,37 @@
+package en.codegym.task.pro.task10.task1007;
+
+/* 
+With a roof or without? That is the question
+*/
+
+public class Bugatti {
+    private String color = "BLACK";
+    private int year = 2020;
+    private String body = "Coupe";
+
+    //write your code here
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
